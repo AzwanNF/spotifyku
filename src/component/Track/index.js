4c723309d513748fe47e542a0ae09b71cc29aa7f
AzwanNF <fadhil.insiderzz@gmail.com>
2022-03-30 19:54:53 +0700

    update Homework 3-1

--- src/component/Track/index.js
+++ src/component/Track/index.js
@@ -1,20 +1,12 @@
-import '../../assets/styles/index.css';
-
-function MusicWrapper({title, url, artists}) {
-    return(
-        <div className="Music">
-            <div className="music-img">
-                <img src={url}/>
-            </div>
-            <div className="music-info">
-                <p className="music-title">{title}</p>
-                <p className="music-artist">{artists}</p>
-            </div>
-            <div>
-                <button className="btn-select">select</button>
-            </div>
-        </div>
+const Track = ({ url, title, artist }) => {
+    return (
+      <div className="card-playlist">
+        <img src={url} alt="Track Playlist" />
+        <h3>{title}</h3>
+        <p>{artist}</p>
+        <button className="btn-primary btn-select">Select</button>
+      </div>
     );
-}
+  };
 
-export default MusicWrapper;
+    export default Track;
