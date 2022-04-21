9beb1790155eed4576840bee7bbb3e7550ac974a
AzwanNF <fadhil.insiderzz@gmail.com>
2022-04-21 22:21:29 +0700

    add homework

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import "./assets/styles/App.css";
-import Home from "./pages/Home";
-
-function App() {
-  return <Home />;
-}
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
-import './assets/styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
