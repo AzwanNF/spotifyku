7dc674a8db6b4fdd25705dcea0e42e33bf37df95
AzwanNF <fadhil.insiderzz@gmail.com>
2022-04-22 23:20:58 +0700

    add final project

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -20,7 +20,7 @@ const config: IConfig = {
   SPOTIFY_BASE_URL: 'https://api.spotify.com/v1',
   SPOTIFY_SCOPE: 'playlist-modify-private',
   RESPONSE_TYPE: 'token',
-  REDIRECT_URI: getHost(),
+  REDIRECT_URI: 'https://spm-nah8omwtz-azwannf.vercel.app'
 };
 
 export default config;
